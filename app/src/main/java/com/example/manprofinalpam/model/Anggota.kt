6dba3716bffb4186bbe4f5e4d51c2cedbbabf725
hi-lufz1 <latifufauzi@gmail.com>
2025-01-21 23:08:07 +0700

    membuat model anggota tim

---
+++ app/src/main/java/com/example/manprofinalpam/model/Anggota.kt
@@ -0,0 +1,28 @@
+package com.example.manprofinalpam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AnggotaResponse (
+    val status: Boolean,
+    val message: String,
+    val data: List<Datum>
+)
+
+@Serializable
+data class Datum (
+    @SerialName("id_anggota")
+    val idAnggota: Int,
+
+    @SerialName("id_tim")
+    val idTim: Int,
+
+    @SerialName("nama_tim")
+    val namaTim: String,
+
+    @SerialName("nama_anggota")
+    val namaAnggota: String,
+
+    val peran: String
+)
