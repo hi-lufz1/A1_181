b76e7d421a82ad5ee774eb54eab6692c0d218529
hi-lufz1 <latifufauzi@gmail.com>
2025-01-25 23:59:59 +0700

    membuat view model list anggota

---
+++ app/src/main/java/com/example/manprofinalpam/ui/viewmodel/anggota/ListAnggotaVM.kt
@@ -0,0 +1,56 @@
+package com.example.manprofinalpam.ui.viewmodel.anggota
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.manprofinalpam.model.dataAnggota
+import com.example.manprofinalpam.repository.AnggotaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class ListAnggotaUIState {
+    data class Success(val anggota: List<dataAnggota>) : ListAnggotaUIState()
+    object Error : ListAnggotaUIState()
+    object Loading : ListAnggotaUIState()
+}
+
+class ListAnggotaVM(private val agt: AnggotaRepository) : ViewModel() {
+    var agtUIState: ListAnggotaUIState by mutableStateOf(ListAnggotaUIState.Loading)
+        private set
+
+    init {
+        getAgt()
+    }
+
+    fun getAgt() {
+        viewModelScope.launch {
+            agtUIState = ListAnggotaUIState.Loading
+            try {
+                val data = agt.getAnggota().data
+                agtUIState = ListAnggotaUIState.Success(data)
+            } catch (e: Exception) {
+                println("Error loading data anggota: ${e.message}")
+                agtUIState = ListAnggotaUIState.Error
+            }
+        }
+    }
+
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun deleteAgt(idAnggota: String) {
+        viewModelScope.launch {
+            try {
+                agt.deleteAnggota(idAnggota)
+            } catch (e: IOException) {
+                ListAnggotaUIState.Error
+            } catch (e: HttpException) {
+                ListAnggotaUIState.Error
+            }
+        }
+    }
+}
